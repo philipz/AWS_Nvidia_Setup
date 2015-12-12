aeef4324ccac217725cd584ee5af42cb06626d37
philipz <philipzheng@gmail.com>
2015-12-12 23:39:35 +0800

    First init

---
+++ README.md
@@ -0,0 +1,3 @@
+#Install Nvidia Driver in Ubuntu
+Just sftp setup_gpu.sh & deviceQuery to AWS GPU instance.
+Then run ./setup_gpu.sh
---
+++ setup_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y nvidia-352 nvidia-settings
+sudo ./deviceQuery
+curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG docker ubuntu
+echo "Finish Install Nvidia Driver & CUDA!";
