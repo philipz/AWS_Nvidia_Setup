a5ed32fd85e8a61639f90c3924df6e1c60d9aac7
philipz <philipzheng@gmail.com>
2015-12-13 00:46:56 +0800

    Fix error

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y nvidia-352 nvidia-settings
-wget https://raw.githubusercontent.com/philipz/AWS_Nvidia_Setup/master/deviceQuery
-chmod +x ./deviceQuery && sudo ./deviceQuery
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker ubuntu
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install linux-headers-$(uname -r)
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get update
+sudo apt-get install -y nvidia-352 nvidia-352-uvm nvidia-modprobe nvidia-settings
+wget https://raw.githubusercontent.com/philipz/AWS_Nvidia_Setup/master/deviceQuery
+chmod +x ./deviceQuery && sudo ./deviceQuery
 echo "Finish Install Nvidia Driver & CUDA!";
