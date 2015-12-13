1de5cdbd1dd48e01f7cacf4ab19bbb1294d0e579
philipz <philipzheng@gmail.com>
2015-12-13 21:02:57 +0800

    Only nvidia-driver

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -2,10 +2,11 @@
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y linux-image-extra-`uname -r` cuda
+sudo apt-get install -y linux-image-extra-`uname -r` build-essential
+sudo apt-get install -y nvidia-352 nvidia-352-uvm nvidia-opencl-icd-352 nvidia-prime nvidia-modprobe nvidia-settings nvidia-352-dev
 wget https://raw.githubusercontent.com/philipz/AWS_Nvidia_Setup/master/deviceQuery
 chmod +x ./deviceQuery && sudo ./deviceQuery
 echo "Finish Install Nvidia Driver & CUDA!";
-#curl -sSL https://get.docker.com/ | sh
-#sudo usermod -aG docker ubuntu
-#echo "Finish Install Docker!";
+curl -sSL https://get.docker.com/ | sh
+sudo usermod -aG docker ubuntu
+echo "Finish Install Docker!";
