aaf12f8f024bd618c31ee94025edb228d4dc6247
philipz <philipzheng@gmail.com>
2015-12-13 17:46:10 +0800

    Fix Error: could not insert 'nvidia_352'

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker ubuntu
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install linux-headers-$(uname -r)
+#sudo apt-get install -y build-essential libstdc++6-4.8-dbg
+sudo apt-get install -y linux-image-extra-`uname -r`
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
 sudo apt-get update
@@ -10,3 +9,6 @@ sudo apt-get install -y nvidia-352 nvidia-352-uvm nvidia-modprobe nvidia-setting
 wget https://raw.githubusercontent.com/philipz/AWS_Nvidia_Setup/master/deviceQuery
 chmod +x ./deviceQuery && sudo ./deviceQuery
 echo "Finish Install Nvidia Driver & CUDA!";
+#curl -sSL https://get.docker.com/ | sh
+#sudo usermod -aG docker ubuntu
+#echo "Finish Install Docker!";
